cacfc60a32e8412fe784adf62840aad1bd4f1e2b
cleberGraciano <cleber_batista2010@hotmail.com>
2024-04-19 17:15:11 -0300

    Subindo aplicação

---
+++ src/main/java/com/projeto/agendadortaferaapi/AgendadorTaferaApiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.agendadortaferaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTaferaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTaferaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/agendadortaferaapi/AgendadorTaferaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.agendadortaferaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTaferaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
